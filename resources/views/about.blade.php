4647cffb55051ab2f4f1924fd02ea2bba14fd991
Mnan <manansakuro@gmail.com>
2024-07-19 06:42:22 +0700

    belajar laravel11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- css style --}}
+    <link rel="stylesheet" href="css/about.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman about</h1>
+    <h2>Hallo Nama saya {{ $nama }}</h2>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Css style --}}
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman Home</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman Home</h1>
+    {{-- alert js --}}
+    <script src="js/alert.js"></script>
+    <img src="img/blog.png" alt="Logo Web">
+</body>
+
+</html>
