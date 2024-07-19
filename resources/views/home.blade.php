6d3348a4746490584f2334805d574f0ec0d7acf6
Mnan <manansakuro@gmail.com>
2024-07-19 11:27:43 +0700

    update template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
     <meta charset="UTF-8">
@@ -10,7 +10,7 @@
     <title>Halaman Home</title>
 </head>
 
-<body>
+<body class="h-full">
     <!--
   This example requires updating your template:
 
