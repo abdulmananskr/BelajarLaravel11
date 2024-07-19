9886e8096259e33ae0d3311978aa030829c8a8e9
Mnan <manansakuro@gmail.com>
2024-07-19 10:33:12 +0700

    membuat tampilan blog

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman Blog</title>
+</head>
+
+<body>
+    <h1>halaman Blog</h1>
+
+    <article>
+        <h3>judul 1</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio aperiam iste error dolorum quis
+            consequatur, unde impedit perspiciatis deleniti tempore ab ut aut nam, molestias consectetur totam dolor non
+            pariatur.</p>
+    </article>
+
+    <article>
+        <h3>judul 2</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio aperiam iste error dolorum quis
+            consequatur, unde impedit perspiciatis deleniti tempore ab ut aut nam, molestias consectetur totam dolor non
+            pariatur.</p>
+    </article>
+</body>
+
+</html>
